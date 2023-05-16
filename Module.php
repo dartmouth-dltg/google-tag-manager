0bd51b4957c59ca5696453079cad063b17eb346d
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2023-05-16 08:25:43 -0400

    typo

--- Module.php
+++ Module.php
@@ -162,6 +162,6 @@ class Module extends AbstractModule {
     return '<!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=' . $code . '"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->"';
+    <!-- End Google Tag Manager (noscript) -->';
   }
 }
